64d3ebaff929ea1720bfe25b8eca3d265e9ca516
trentb10 <trentbmin@gmail.com>
2023-03-17 21:27:52 -0400

    Refactored parts of the code that made api requests, now commands will use a method dedicated for it

--- Commands/General.cs
+++ Commands/General.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using System.Web;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
@@ -9,6 +10,49 @@ public class General : BaseCommandModule
 
   static readonly HttpClient client = new HttpClient();
   private const string lastfmURL = "http://ws.audioscrobbler.com/2.0/?method=";
+
+  public async Task<string> RequestData
+  (
+    string method,
+    string user
+  )
+  {
+    // Get API key
+    ApiKeyring lastfmKey = JsonConvert.DeserializeObject<ApiKeyring>
+    (
+      File.ReadAllText("config\\apikeys.json")
+    );
+
+    // Set parameters
+    var param = HttpUtility.ParseQueryString(string.Empty);
+    param["method"] = method;
+    param["user"] = user;
+    param["api_key"] = lastfmKey.apiKey.lastfm;
+    param["format"] = "json";
+
+    // Build URL
+    var urlBuilder = new UriBuilder(lastfmURL);
+    urlBuilder.Port = -1;
+    urlBuilder.Query = param.ToString();
+    string reqUrl = urlBuilder.ToString();
+
+    // Request data
+    string resData = "";
+    try
+    {
+      using HttpResponseMessage res = await client.GetAsync(reqUrl);
+      res.EnsureSuccessStatusCode();
+
+      resData = await res.Content.ReadAsStringAsync();
+    }
+    catch (HttpRequestException e)
+    {
+      Console.WriteLine(e);
+      resData = "FAILED";
+    }
+
+    return await Task.FromResult(resData);
+  }
 
   [Command("greet")]
   public async Task RGreet(CommandContext ctx)
@@ -23,34 +67,25 @@ public class General : BaseCommandModule
   [Aliases("np")]
   public async Task RNowPlaying(CommandContext ctx)
   {
-    // ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=rj&api_key=YOUR_API_KEY&format=json
-
-    // Get API key
-    ApiKeyring lastfmKey = JsonConvert.DeserializeObject<ApiKeyring>
-    (
-      File.ReadAllText("config\\apikeys.json")
-    );
-
-    // Declare urls for api call
-    string paramMethod = "user.getrecenttracks",
-           paramUser = "&user=tm206",
-           paramApiKey = $"&api_key={lastfmKey.apiKey.lastfm}";
-
     // Request data
-    try 
+    string resData = await RequestData
+                           (
+                            "user.getrecenttracks",
+                            "tm206"
+                           );
+    
+    // Display data
+    if (resData == "FAILED")
+    {
+      await ctx.Channel.SendMessageAsync("Something went wrong...");
+    }
+    else
     {
       // Get user information
       string userName = ctx.Message.Author.Username;
       string userIcon = ctx.Message.Author.AvatarUrl;
-
-      // Get latest track aka now playing
-      using HttpResponseMessage res = await client.GetAsync
-      (
-        lastfmURL + paramMethod + paramUser + paramApiKey + "&format=json"
-      );
-      res.EnsureSuccessStatusCode();
-      string resData = await res.Content.ReadAsStringAsync();
-
+      
+      // Get recent track
       LastFM_User_RecentTracks tracks = JsonConvert.DeserializeObject<LastFM_User_RecentTracks>(resData);
       
       var currentTrack = tracks.recenttracks.track[0];
@@ -76,52 +111,37 @@ public class General : BaseCommandModule
           albumCoverURL
         )
       );
-      
-    } 
-    catch (HttpRequestException e)
-    {
-      await ctx.Channel.SendMessageAsync("Something went wrong...");
-      Console.WriteLine(e);
     }
-
   }
 
   [Command("toptracks")]
   public async Task RTopTracks(CommandContext ctx)
   {
-    // Get API key
-    ApiKeyring lastfmKey = JsonConvert.DeserializeObject<ApiKeyring>
-    (
-      File.ReadAllText("config\\apikeys.json")
-    );
-
-    // Declare urls for api call
-    string paramMethod = "user.gettoptracks",
-           paramUser = "&user=tm206",
-           paramApiKey = $"&api_key={lastfmKey.apiKey.lastfm}";
-
     // Request data
-    try
+    // Default arg is 10 tracks
+    string resData = await RequestData
+                           (
+                             "user.gettoptracks",
+                             "tm206"
+                           );
+
+    // Display data
+    if (resData == "FAILED")
+    {
+      await ctx.Channel.SendMessageAsync("Something went wrong...");
+    }
+    else
     {
       // Get user information
       string userName = ctx.Message.Author.Username;
       string userIcon = ctx.Message.Author.AvatarUrl;
-
-      // Get top tracks
-      // Default arg is 10 tracks
-      using HttpResponseMessage res = await client.GetAsync
-      (
-        lastfmURL + paramMethod + paramUser + paramApiKey + "&format=json"
-      );
-      res.EnsureSuccessStatusCode();
-      string resData = await res.Content.ReadAsStringAsync();
-
+      
       LastFM_User_TopTracks tracks = JsonConvert.DeserializeObject<LastFM_User_TopTracks>(resData);
       var track = tracks.toptracks.track;
 
       // Spotlight top track
       string toptrack = $"**1\u0020|\u0020{track[0].name} \u2013 {track[0].artist.name}** ({track[0].playcount} plays)";
-      
+
       string topTrackAlbumCover = "";
 
       foreach (var a in track[0].image)
@@ -132,9 +152,9 @@ public class General : BaseCommandModule
       // Get rest of tracks
       List<string> topTracks = new List<string>();
 
-      for(int i = 1; i < 10; i++)
+      for (int i = 1; i < 10; i++)
       {
-        string title = $"**{i+1}\u0020|\u0020{track[i].name} \u2013 {track[i].artist.name}** ({track[i].playcount} plays)";
+        string title = $"**{i + 1}\u0020|\u0020{track[i].name} \u2013 {track[i].artist.name}** ({track[i].playcount} plays)";
         topTracks.Add(title);
       }
 
@@ -155,13 +175,6 @@ public class General : BaseCommandModule
         topTrackAlbumCover,
         topTracksList
       ));
-
-    } catch (HttpRequestException e)
-    {
-      await ctx.Channel.SendMessageAsync("Something went wrong...");
-      Console.WriteLine(e);
     }
-    
-
   }
 }
